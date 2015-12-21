858b588006ac2e53bf4e80295fd0f294ff99c868
Cloudox <cloudox@foxmail.com>
2015-12-21 10:47:50 +0800

    Initial Commit

---
+++ SubStringDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubStringDemo
+//
+//  Created by csdc-iMac on 15/12/21.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SubStringDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SubStringDemo
+//
+//  Created by csdc-iMac on 15/12/21.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
